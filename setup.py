42b48283355f6dc167bcf684f574dc68a5b5c0d5
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-07-06 20:44:33 +0100

    Initialize project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project creates an opensource visualization of Monkey pox epidermic around the world, while answering some basic frequently asked questions about the epidermic',
+    author='Ajala Marvellous O.',
+    license='MIT',
+)
