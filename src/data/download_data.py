edb28de5bb3f19fef2d34d5c651204eebbb2f8d5
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-07-07 23:08:08 +0100

    Add get_data function to download the data

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -5,3 +5,15 @@ import logging
 log_fmt = "%(asctime)s - [%(name)s] - %(levelname)s : %(message)s"
 logging.BasicConfig(level=logging.INFO, format=log_fmt)
 logger = logging.getLogger()
+
+
+def get_data(url):
+    """Retrieves the data from the internet"""
+    data = requests.get(url)
+    # Confirms that correct data is returned
+    try:
+        data.raise_for_status()
+        logger.info("File downloaded safely")
+        return data
+    except Exception as exec:
+        logger.error(f"File download was not successful due to {exec}")
