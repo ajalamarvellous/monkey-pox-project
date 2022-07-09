534b51eb879983dbb70dbfc5c292b29498c4923a
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-07-09 17:26:04 +0100

    Add test for get_data function

--- tests/test_download_data.py
+++ tests/test_download_data.py
@@ -13,8 +13,13 @@ def url():
     return "https://www.google.com/"
 
 
-def test_get_data():
-    pass
+def test_get_data(url):
+    """Tests get_data function"""
+    # Using .run because it is a prefect task and it is running outside prefect
+    # flow manager, thus need .run method to function
+    data = dd.get_data.run(url)
+    assert isinstance(data, list)
+    assert isinstance(data[0], str)
 
 
 def test_get_name():
