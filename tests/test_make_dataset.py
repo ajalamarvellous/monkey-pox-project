75ced20383c826bf13ed1c0ec2b7cbed65af9558
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-07-12 01:43:49 +0100

    Add file_name func test

--- tests/test_make_dataset.py
+++ tests/test_make_dataset.py
@@ -1,16 +1,25 @@
 import os
+import sys
 
 import pytest
 
+# Importing the file we want to test
+sys.path.insert(0, os.getcwd() + "/src/data")
+import make_dataset as md  # noqa
 
-@pytest.fixtures
+
+@pytest.fixture
 def dir_name():
-    location = os.path.abspath(__file__)
-    return os.path.join([location.parents[0], "data", "raw"])
+    location = os.path.dirname(__file__)
+    return os.path.join(location, "..", "data", "raw")
 
 
-def test_file_address():
-    pass
+def test_file_address(dir_name):
+    assert os.path.exists(dir_name)
+    address = md.file_address(dir_name, "latest.csv")
+    assert os.path.exists(address)
+    none_yet = md.file_address(dir_name, "none_yet.csv")
+    assert none_yet == os.path.join(dir_name, "none_yet.csv")
 
 
 def test_get_file():
