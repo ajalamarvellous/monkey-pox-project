a9b6f0ab5970edd65182766c79759680878f90d9
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-08-04 13:47:16 +0100

    Refactor page
    Refactor the code to remove the csv and requests and instead download pandas and save to parquet

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,11 +1,9 @@
-import csv
 import logging
 import os
-from datetime import datetime, timedelta
 
-import requests
-from prefect import Flow, task
+import pandas as pd
 
+# from prefect import Flow, task
 # from prefect.schedules import IntervalSchedule
 
 log_fmt = "%(name)s %(asctime)s - [%(funcName)s] - %(levelname)s : %(message)s"
@@ -15,38 +13,18 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Add task to encapsulate our function and retry 10 if it fails
-@task(max_retries=10, retry_delay=timedelta(seconds=10))
-def get_data(url):
-    """Retrieves the data from the internet"""
-    data = requests.get(url)
-    # Confirms that correct data is returned
-    try:
-        data.raise_for_status()
-        logger.info("File downloaded safely")
-        return data.text.split("\n")
-    except Exception as error:
-        logger.exception(f"File download was not successful due to {error}")
-
-
-@task
-def get_name(url):
-    """Retrieves the name of the file from the url address"""
-    return url.split("/")[-1]
-
-
-@task
-def save_file(data, file_name):
+# @Flow
+def main(url):
     """Saves the data to into the file"""
-    address = os.path.join(
-        "data", "raw", datetime.now().ctime() + " " + file_name
-    )  # noqa
-    file = open(address, "w+")
-    csv_writer = csv.writer(file)
-    for line in data:
-        csv_writer.writerow(line.split(","))
-    file.close()
-    logger.info("File written and saved successfully")
+    file_name = url.split("/")[-1]
+    file_name = f"{file_name.split('.')[0]}.parquet"
+    # address = os.path.join(
+    #     "data", "raw", datetime.now().ctime() + " " + file_name
+    # )
+    address = os.path.join("data", "raw", file_name)
+    df = pd.read_csv(url)
+    df.to_parquet(address)
+    # logger.info("File written and saved successfully")
 
 
 # TO FIX:
@@ -54,20 +32,12 @@ def save_file(data, file_name):
 # scheduler = IntervalSchedule(interval=timedelta(days=1))
 
 
-def main():
-    with Flow(name="Data download pipeline") as flow:
-        url_list = [
-            "https://raw.githubusercontent.com/globaldothealth/monkeypox/main/timeseries-country-confirmed.csv",  # noqa
-            "https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv",  # noqa
-        ]
-        for url in url_list:
-            data = get_data(url)
-            filename = get_name(url)
-            save_file(data=data, file_name=filename)
-        logger.info("Task completed...")
-    return flow
-
-
 if __name__ == "__main__":
-    prefect_flow = main()
-    prefect_flow.run()
+    url_list = [
+        "https://raw.githubusercontent.com/globaldothealth/monkeypox/main/timeseries-country-confirmed.csv",  # noqa
+        "https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv",  # noqa
+    ]
+    for url in url_list:
+        main(url)
+        # prefect_flow =
+        # prefect_flow.run()
