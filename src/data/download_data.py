9725be46551537a6ed26461f81d58046b77f4828
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-08-04 21:54:07 +0100

    Add prefect flow and datestamp files

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,10 +1,12 @@
 import logging
 import os
+from datetime import datetime
 
 import pandas as pd
+from prefect import Flow
 
-# from prefect import Flow, task
 # from prefect.schedules import IntervalSchedule
+
 
 log_fmt = "%(name)s %(asctime)s - [%(funcName)s] - %(levelname)s : %(message)s"
 logging.basicConfig(
@@ -13,18 +15,21 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# @Flow
 def main(url):
     """Saves the data to into the file"""
-    file_name = url.split("/")[-1]
-    file_name = f"{file_name.split('.')[0]}.parquet"
-    # address = os.path.join(
-    #     "data", "raw", datetime.now().ctime() + " " + file_name
-    # )
-    address = os.path.join("data", "raw", file_name)
-    df = pd.read_csv(url)
-    df.to_parquet(address)
-    # logger.info("File written and saved successfully")
+    with Flow(name="Data-download-etl") as flow:
+
+        file_name = url.split("/")[-1]
+        file_name = f"{file_name.split('.')[0]}.parquet"
+
+        date = datetime.strftime(datetime.now(), "%Y-%m-%d")
+        address = os.path.join("data", "raw", date + "-" + file_name)
+
+        logger.info(f"Dowloading file from {url}....")
+        df = pd.read_csv(url)
+        df.to_parquet(address)
+        logger.info("File written and saved successfully")
+        return flow
 
 
 # TO FIX:
@@ -39,5 +44,3 @@ if __name__ == "__main__":
     ]
     for url in url_list:
         main(url)
-        # prefect_flow =
-        # prefect_flow.run()
