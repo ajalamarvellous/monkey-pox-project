c3df6842d1ef57098e2e6337e64ef7c62ba45f60
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-08-07 23:55:57 +0100

    Add click to add commandline prompt for url and file location

--- src/data/countries_meta.py
+++ src/data/countries_meta.py
@@ -4,18 +4,22 @@ url = "https://meta.m.wikimedia.org/wiki/List_of_countries_by_regional_classific
 
 from pathlib import Path
 
+import click
 import pandas as pd
 
 LOCATION = Path("data", "raw")
 
 
-def get_countries(url: str):
+@click.command()
+@click.argument("table_url")
+@click.argument("output_filepath", type=click.Path())
+def get_countries(table_url: str, output_filepath: str):
     """
     This function downloads data from a table in a wikipedia page online and
     saves it to a dataframe
     """
-    data = pd.read_html(url)
+    data = pd.read_html(table_url)
     df = pd.DataFrame(data[0])
-    filename = Path(LOCATION, "countries_meta.parquet")
+    filename = Path(output_filepath, "countries_meta.parquet")
     df.to_parquet(filename)
     return df
