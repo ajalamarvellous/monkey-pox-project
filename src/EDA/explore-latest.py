6e5b8374a2b98142e367685e6923e9ddb33d1a99
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-08-11 23:59:37 +0100

    EDA on time and location columns

--- src/EDA/explore-latest.py
+++ src/EDA/explore-latest.py
@@ -11,3 +11,50 @@ df.head()
 df.info()
 df.describe(include="all")
 df.iloc[0]
+
+df.columns
+
+
+df = df.drop(["Source_V", "Source_VI", "Source_VII"], axis=1)
+
+df["Date_onset"] = pd.to_datetime(df["Date_onset"])
+df["Date_confirmation"] = pd.to_datetime(df["Date_confirmation"])
+df["Date_hospitalisation"] = pd.to_datetime(df["Date_hospitalisation"])
+df["Date_onset"].min()
+df["Date_confirmation"].min()
+df[df["Date_confirmation"] == df["Date_confirmation"].min()]
+
+
+df.ID.nunique()
+
+df.Location.nunique()
+
+df.Location.value_counts()
+df.City.value_counts()
+df.sample(10)
+
+df[
+    df[
+        [
+            "Date_hospitalisation",
+            "Isolated (Y/N/NA)",
+            "Date_isolation",
+            "Outcome",
+            "Contact_comment",
+            "Contact_ID",
+            "Contact_location",
+            "Travel_history (Y/N/NA)",
+            "Travel_history_entry",
+            "Travel_history_start",
+            "Travel_history_location",
+            "Travel_history_country",
+            "Genomics_Metadata",
+            "Confirmation_method",
+        ]
+    ].notnull()
+]
+df[
+    df["Genomics_Metadata"].notnull() & (df["Travel_history_country"].notnull())  # noqa
+].sample(  # noqa
+    10
+)
