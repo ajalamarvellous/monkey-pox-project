955ee32e2828ddebefb57031ad05ea60bf18681c
Ajala Marvellous <ajalaoluwamayowa00@gmail.com>
2022-09-16 17:24:06 +0100

    Refactor code and add prefect scheduler

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -3,10 +3,8 @@ import os
 from datetime import datetime
 
 import pandas as pd
-from prefect import Flow
-
-# from prefect.schedules import IntervalSchedule
-
+from prefect import flow, task
+from prefect.schedules import CronSchedule
 
 log_fmt = "%(name)s %(asctime)s - [%(funcName)s] - %(levelname)s : %(message)s"
 logging.basicConfig(
@@ -15,26 +13,33 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+# Schedule for how often to run the tasks
+scheduler = CronSchedule(cron="0 0 * * 0")
+
+
+def get_filename(address):
+    """Returns the name the new ile will ne called"""
+
+
+@task(retries=10, retry_delay_seconds=30)
+def get_file(url, address):
+    """Get csv file and save locally as .parquet"""
+    logger.info(f"Dowloading file from {url}....")
+    df = pd.read_csv(url)
+    df.to_parquet(address)
+    logger.info("File written and saved successfully")
+
+
+@flow(name="Data-download-etl", schedule=scheduler)
 def main(url):
     """Saves the data to into the file"""
-    with Flow(name="Data-download-etl") as flow:
+    file_name = url.split("/")[-1]
+    file_name = f"{file_name.split('.')[0]}.parquet"
 
-        file_name = url.split("/")[-1]
-        file_name = f"{file_name.split('.')[0]}.parquet"
+    date = datetime.strftime(datetime.now(), "%Y-%m-%d")
+    address = os.path.join("data", "raw", date + "-" + file_name)
 
-        date = datetime.strftime(datetime.now(), "%Y-%m-%d")
-        address = os.path.join("data", "raw", date + "-" + file_name)
-
-        logger.info(f"Dowloading file from {url}....")
-        df = pd.read_csv(url)
-        df.to_parquet(address)
-        logger.info("File written and saved successfully")
-        return flow
-
-
-# TO FIX:
-# Schedule for how often to run the tasks
-# scheduler = IntervalSchedule(interval=timedelta(days=1))
+    get_file(url, address)
 
 
 if __name__ == "__main__":
@@ -45,3 +50,5 @@ if __name__ == "__main__":
     for url in url_list:
         flow = main(url)
         flow.run()
+
+# TO DO: Add the urls to a local env file and load with dotenv
